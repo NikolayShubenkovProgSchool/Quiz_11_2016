3eaa9aea56257911c14c027091a72ff2aaae5d9c
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-12-03 13:07:11 +0300

    load Quiz from app resources

--- Quiz/DataLoader.swift
+++ Quiz/DataLoader.swift
@@ -10,6 +10,32 @@ import Foundation
 
 class DataLoader {
     
-    
-    
+    func loadQuizWith(name:String)->Quiz?
+    {
+        //попытаемся найти путь, по которому лежит наш файл с викториной
+        guard let pathToQuestion = Bundle.main.path(forResource: name,
+                                                    ofType: "json") else {
+                                                        return nil
+        }
+        
+        //попробуем прочитать сырые данные из файл
+        guard let data = NSData(contentsOfFile: pathToQuestion) else {
+            return nil
+        }
+        
+        //мы получили что-то
+        guard let quizData = try? JSONSerialization.jsonObject(with: data as Data,
+                                                          options: []) else {
+                                                            return nil
+        }
+        
+        //привели полученный объект к нужному нам типу для создания нашей модели
+        guard let json = quizData as? [String:Any] else {
+            return nil
+        }
+        
+        guard let quiz = Quiz(json: json) else { return nil }
+        
+        return quiz
+    }
 }
